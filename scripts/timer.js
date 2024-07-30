ac59b028fdec6b3240b6309ab9010b188f17c689
Param-10 <bheleparamveer@gmail.com>
2024-07-30 14:03:01 +0530

    chore: Refactor popup styling and structure

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,52 +1,52 @@
 let currentTimer;
 
 function startFocusSession(duration) {
-  clearTimeout(currentTimer);
-  let endTime = Date.now() + duration * 1000;
+    clearTimeout(currentTimer);
+    let endTime = Date.now() + duration * 1000;
 
-  function updateTimer() {
-    let timeLeft = Math.round((endTime - Date.now()) / 1000);
-    
-    if (timeLeft <= 0) {
-      chrome.runtime.sendMessage({action: "focusComplete"});
-    } else {
-      updateTimerDisplay(timeLeft);
-      currentTimer = setTimeout(updateTimer, 1000);
+    function updateTimer() {
+        let timeLeft = Math.round((endTime - Date.now()) / 1000);
+
+        if (timeLeft <= 0) {
+            chrome.runtime.sendMessage({ action: "focusComplete" });
+        } else {
+            updateTimerDisplay(timeLeft);
+            currentTimer = setTimeout(updateTimer, 1000);
+        }
     }
-  }
 
-  updateTimer();
+    updateTimer();
 }
 
 function startBreak(duration) {
-  clearTimeout(currentTimer);
-  let endTime = Date.now() + duration * 1000;
+    clearTimeout(currentTimer);
+    let endTime = Date.now() + duration * 1000;
 
-  function updateTimer() {
-    let timeLeft = Math.round((endTime - Date.now()) / 1000);
-    
-    if (timeLeft <= 0) {
-      chrome.runtime.sendMessage({action: "breakComplete"});
-    } else {
-      updateTimerDisplay(timeLeft);
-      currentTimer = setTimeout(updateTimer, 1000);
+    function updateTimer() {
+        let timeLeft = Math.round((endTime - Date.now()) / 1000);
+
+        if (timeLeft <= 0) {
+            chrome.runtime.sendMessage({ action: "breakComplete" });
+        } else {
+            updateTimerDisplay(timeLeft);
+            currentTimer = setTimeout(updateTimer, 1000);
+        }
     }
-  }
 
-  updateTimer();
+    updateTimer();
 }
 
 function updateTimerDisplay(timeLeft) {
-  let minutes = Math.floor(timeLeft / 60);
-  let seconds = timeLeft % 60;
-  let display = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  chrome.runtime.sendMessage({action: "updateTimerDisplay", display: display});
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let display = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    chrome.runtime.sendMessage({ action: "updateTimerDisplay", display: display });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "startFocus") {
-    startFocusSession(request.duration);
-  } else if (request.action === "startBreak") {
-    startBreak(request.duration);
-  }
+    if (request.action === "startFocus") {
+        startFocusSession(request.duration);
+    } else if (request.action === "startBreak") {
+        startBreak(request.duration);
+    }
 });
